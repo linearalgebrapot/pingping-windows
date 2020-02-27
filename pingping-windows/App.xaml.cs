ba2eb544a330d99cc2f3f5f8d73f2e6f69c91ee1
박현준 <evoluti0n@hanyang.ac.kr>
2020-02-27 16:46:14 +0900

    초기 설정

---
+++ pingping-windows/App.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace pingping_windows
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+    }
+}
---
+++ pingping-windows/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace pingping_windows
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
